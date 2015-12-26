2e7535fab78ce9fa99d92317969ebe7d21143415
scoty755 <scoty755@gmail.com>
2015-12-26 12:31:55 +0900

    Add 64bit option

--- kallsyms_in_memory.h
+++ kallsyms_in_memory.h
@@ -6,21 +6,21 @@
 
 typedef struct _kallsyms kallsyms;
 
-kallsyms *     kallsyms_in_memory_init          (unsigned long *mem, size_t len);
+kallsyms *     kallsyms_in_memory_init          (unsigned long *mem, size_t len, int is64bit);
 void           kallsyms_in_memory_free          (kallsyms *kallsyms);
-unsigned long  kallsyms_in_memory_lookup_name   (kallsyms *kallsyms, const char *name);
+unsigned long  kallsyms_in_memory_lookup_name   (kallsyms *kallsyms, const char *name, int is64bit);
 bool           kallsyms_in_memory_lookup_names  (kallsyms *kallsyms, const char *name,
-                                                 unsigned long *addresses, size_t n_addresses);
-const char    *kallsyms_in_memory_lookup_address(kallsyms *kallsyms, unsigned long address);
+                                                 unsigned long *addresses, size_t n_addresses, int is64bit);
+const char    *kallsyms_in_memory_lookup_address(kallsyms *kallsyms, unsigned long address, int is64bit);
 
 bool is_address_in_kallsyms_table(kallsyms *kallsyms, void *mapped_address);
 
 void kallsyms_in_memory_set_verbose(bool verbose);
-void kallsyms_in_memory_print_all(kallsyms *kallsyms);
-void kallsyms_in_memory_print_all_to_file(kallsyms *kallsyms, FILE *fp);
+void kallsyms_in_memory_print_all(kallsyms *kallsyms, int is64bit);
+void kallsyms_in_memory_print_all_to_file(kallsyms *kallsyms, FILE *fp, int is64bit);
 
 #endif /* __KALLSYMSPRINT_H__ */
 
 /*
-vi:ts=2:nowrap:ai:expandtab:sw=2
-*/
+ vi:ts=2:nowrap:ai:expandtab:sw=2
+ */
